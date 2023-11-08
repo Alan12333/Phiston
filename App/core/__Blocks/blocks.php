96820e006dfd204c2180228381346fdd054f5b5d
Alan12333 <alan.guzman12333@gmail.com>
2023-11-08 16:40:23 -0600

    Actualizacion de exepciones

--- App/core/__Blocks/blocks.php
+++ App/core/__Blocks/blocks.php
@@ -8,35 +8,30 @@ class Blocks
     function __construct($controller, $action)
     {
         $this->DisplayErrors();
-        $this->controller=$controller;
-        $this->action=$action;
+        $this->controller = $controller;
+        $this->action = $action;
         ThrowException::setErrorHandler();
     }
     function Call()
     {
-        try
-        {
-            $url='App/controllers/'.$this->controller.'_Controller.php';
+        if ($this->display === true) {
+            $url = 'App/controllers/' . $this->controller . '_Controller.php';
             $urls = scandir("App/controllers");
             $validacion = false;
             foreach ($urls as $key => $value) {
-                if($this->controller.'_Controller.php'==$value)
-                {
-                    $validacion=true;
+                if ($this->controller . '_Controller.php' == $value) {
+                    $validacion = true;
                 }
             }
-            if($validacion==true)
-            {
+            if ($validacion == true) {
                 require_once($url);
                 $this->action = $this->SpecialCase();
-                $classname= $this->controller.'_Controller';
+                $classname = $this->controller . '_Controller';
                 $newclass = new $classname;
-                if(method_exists($newclass, $this->action))
-                {
-                    $data = $newclass->{ $this->action}();
-                
-                    if(is_object($data) == true || is_array($data)==true)
-                    {
+                if (method_exists($newclass, $this->action)) {
+                    $data = $newclass->{$this->action}();
+
+                    if (is_object($data) == true || is_array($data) == true) {
                         header("Content-Type:application/json");
                         header('Access-Control-Allow-Origin: *');
                         header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
@@ -44,33 +39,63 @@ class Blocks
                         header("Allow: GET, POST, OPTIONS, PUT, DELETE");
                         echo json_encode($data);
                     }
-                }
-                else
-                {
-                    if($this->display==true)
-                    {
-                        echo "No existe el metodo en la clase ".$this->controller;
-                    }
-                    else
-                    {
-                        require_once  dirname(__FILE__)."/Folder/Views/404.php";
+                } else {
+                    if ($this->display == false) {
+                        echo "No existe el metodo en la clase " . $this->controller;
+                    } else {
+                        require_once  dirname(__FILE__) . "/Folder/Views/404.php";
                     }
                 }
-            }
-            else
-            {
-                if($this->display==true)
-                {
-                    echo "No existe el controlador ni la clase ".$this->controller;
-                }
-                else
-                {
-                    require_once  dirname(__FILE__)."/Folder/Views/404.php";
+            } else {
+                if ($this->display == false) {
+                    echo "No existe el controlador ni la clase " . $this->controller;
+                } else {
+                    require_once  dirname(__FILE__) . "/Folder/Views/404.php";
                 }
             }
-        } catch (Error $err)
-        {
-            return ThrowException::CreateAlert($err);
+        } else {
+            try {
+                $url = 'App/controllers/' . $this->controller . '_Controller.php';
+                $urls = scandir("App/controllers");
+                $validacion = false;
+                foreach ($urls as $key => $value) {
+                    if ($this->controller . '_Controller.php' == $value) {
+                        $validacion = true;
+                    }
+                }
+                if ($validacion == true) {
+                    require_once($url);
+                    $this->action = $this->SpecialCase();
+                    $classname = $this->controller . '_Controller';
+                    $newclass = new $classname;
+                    if (method_exists($newclass, $this->action)) {
+                        $data = $newclass->{$this->action}();
+
+                        if (is_object($data) == true || is_array($data) == true) {
+                            header("Content-Type:application/json");
+                            header('Access-Control-Allow-Origin: *');
+                            header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+                            header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+                            header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+                            echo json_encode($data);
+                        }
+                    } else {
+                        if ($this->display == false) {
+                            echo "No existe el metodo en la clase " . $this->controller;
+                        } else {
+                            require_once  dirname(__FILE__) . "/Folder/Views/404.php";
+                        }
+                    }
+                } else {
+                    if ($this->display == false) {
+                        echo "No existe el controlador ni la clase " . $this->controller;
+                    } else {
+                        require_once  dirname(__FILE__) . "/Folder/Views/404.php";
+                    }
+                }
+            } catch (Error $err) {
+                return ThrowException::CreateAlert($err);
+            }
         }
     }
 
@@ -79,28 +104,19 @@ class Blocks
         $string = $this->action;
         $expl = explode("-", $string);
         $cont = count($expl);
-        $newstring="";
-        
-        if($cont>=2)
-        {
-            for($i=0; $i<$cont; $i++)
-            {
-                if($i==0)
-                {
-                    $newstring=$expl[$i];
+        $newstring = "";
+
+        if ($cont >= 2) {
+            for ($i = 0; $i < $cont; $i++) {
+                if ($i == 0) {
+                    $newstring = $expl[$i];
+                } else if ($i == $cont - 1) {
+                    $newstring = $newstring . "_" . $expl[$i];
+                } else {
+                    $newstring = $newstring . "_" . $expl[$i] . "_";
                 }
-                else if($i==$cont-1)
-                {
-                    $newstring = $newstring."_".$expl[$i];
-                }
-                else
-                {
-                    $newstring = $newstring."_".$expl[$i]."_";
-                }   
             }
-        }
-        else
-        {
+        } else {
             $newstring = $this->action;
         }
         return $newstring;
@@ -109,17 +125,10 @@ class Blocks
     {
         $data = file_get_contents("config.json");
         $products = json_decode($data, true);
-        if($products['production']==true)
-        {
-            $this->display=true;
-        }
-        else
-        {
-            $this->display=false;
+        if ($products['production'] == true) {
+            $this->display = true;
+        } else {
+            $this->display = false;
         }
     }
 }
-
-
-
-?>
